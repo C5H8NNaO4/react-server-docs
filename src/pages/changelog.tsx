310f89e2d7fc63316ab13d5d63e2bbe23298a703
Moritz Roessler <morrossl@publicisgroupe.net>
2023-05-04 10:06:29 +0200

    feat: add commits to changelog

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -1,8 +1,18 @@
-import { Paper, Container } from '@mui/material';
+import {
+  Paper,
+  Container,
+  Card,
+  CardHeader,
+  ListItem,
+  ListItemText,
+  Grid,
+} from '@mui/material';
 
 import { Markdown } from '../components/Markdown';
 import { getRawPath } from '../components/CollabEditButton';
 import { Navigation } from '../components/NavigationButton';
+import { useEffect, useState } from 'react';
+import Github from 'github-api';
 
 const PAGE_SRC = 'src/pages/changelog.md';
 
@@ -21,8 +31,53 @@ export const ChangeLog = () => {
         }}
       >
         <Markdown src={getRawPath(PAGE_SRC)}>*Loading*</Markdown>
+        <Markdown># Commits</Markdown>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={4}>
+            <Commits repo={'state-less/react-server'} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Commits repo={'state-less/react-client'} />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Commits repo={'C5H8NNaO4/react-server-docs'} />
+          </Grid>
+        </Grid>
         <Navigation />
       </Paper>
     </Container>
+  );
+};
+
+const Commits = ({ repo }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const org = repo.split('/')[0].replace('@', '');
+  const rep = repo.split('/')[1];
+  useEffect(() => {
+    (async () => {
+      const gh = new Github();
+      gh.getRepo(org, rep).listCommits((err, commits) => {
+        setData(commits);
+        setLoading(false);
+      });
+    })();
+  });
+
+  return (
+    <Card sx={{ height: '100%' }}>
+      <CardHeader title={repo}></CardHeader>
+      {data.map((commit) => {
+        return (
+          <ListItem dense>
+            <ListItemText
+              primary={commit?.commit?.message}
+              secondary={commit?.author?.login}
+            ></ListItemText>
+          </ListItem>
+        );
+      })}
+    </Card>
   );
 };
