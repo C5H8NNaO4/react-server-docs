45a77e8989d988c99e130c585707d2ab78525d73
Moritz Roessler <moritz.roessler@gmail.com>
2023-07-22 18:24:46 +0200

    fix(Lists): fix undefined filter

--- src/server-components/examples/Lists.tsx
+++ src/server-components/examples/Lists.tsx
@@ -235,7 +235,7 @@ export const MyLists = (props) => {
     {}
   );
   const [_optimisticOrder, setOptimisticOrder] = useState(items);
-  const optimisticOrder = _optimisticOrder.filter(
+  const optimisticOrder = _optimisticOrder?.filter(
     (id) => lkp[id] && (showArchived || !lkp[id].props.archived)
   );
   useEffect(() => {
