34a0eec4497c64bbf4b8a762bb6101de4fafe0ce
Moritz Roessler <moritz.roessler@gmail.com>
2023-08-21 10:51:30 +0200

    fix: update service worker

--- src/service-worker2.js
+++ src/service-worker2.js
@@ -38,7 +38,7 @@ self.addEventListener('notificationclick', (event) => {
   });
   if (action === 'complete') {
     try {
-      const promise = fetch(`http://localhost:4000/todos/${id}/toggle`, {
+        const promise = fetch(`http://backend.state-less.cloud/todos/${id}/toggle`, {
         method: 'GET',
         headers: {
           Authorization: bearer,
