c7d2f781124616390512c6151a4ebd3f141adb41
Moritz Roessler <moritz.roessler@gmail.com>
2024-01-27 00:58:20 +0100

    feat: add edit + delete for answers

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -84,6 +84,7 @@ export const Markdown = ({
         minHeight: optimisticHeight,
         width: '100%',
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
       }}
     >
