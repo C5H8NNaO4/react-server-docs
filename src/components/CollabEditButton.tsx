4f549886a23d570e21a09925ed7884b4fce11212
Moritz Roessler <morrossl@publicisgroupe.net>
2024-02-26 13:21:13 +0100

    feat: stage content from github

--- src/components/CollabEditButton.tsx
+++ src/components/CollabEditButton.tsx
@@ -1,13 +1,14 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Link } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import { GH_CONTENT_BRANCH, GH_CONTENT_REPO } from '../lib/config';
 
 export const getRawPath = (path: string) => {
-  return `https://raw.githubusercontent.com/state-less/react-server-docs-md/master/${path}`;
+  return `https://raw.githubusercontent.com/${GH_CONTENT_REPO}/${GH_CONTENT_BRANCH}/${path}`;
 };
 
 export const getGHPath = (path: string) => {
-  return `https://github.com/state-less/react-server-docs-md/blob/master/${path}`;
+  return `https://github.com/${GH_CONTENT_REPO}/blob/${GH_CONTENT_BRANCH}/${path}`;
 };
 
 export const CollabEditButton = ({ to }: { to: string }) => {
