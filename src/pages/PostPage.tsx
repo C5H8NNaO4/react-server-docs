501fa58d53a68a7e863ffdb9efc7385c7c19ca68
Moritz Roessler <morrossl@publicisgroupe.net>
2024-03-01 15:51:44 +0100

    fix: correct forum base path in posts page

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,4 +1,4 @@
 import PostsPage from '@state-less/leap-frontend/dist/forum/post';
 import { FORUM_KEY } from '../lib/config';
 
-export default () => <PostsPage basePath="/" forumKey={FORUM_KEY} />;
+export default () => <PostsPage basePath="" forumKey={FORUM_KEY} />;
