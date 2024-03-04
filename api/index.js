2277312d8cb99fc076e1b96c5fc9e37418b76376
Moritz Roessler <morrossl@publicisgroupe.net>
2024-03-04 04:23:32 +0100

    wip

--- api/index.js
+++ api/index.js
@@ -7,27 +7,31 @@ import { createServer as createViteServer } from 'vite';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 5173;
+const IS_PROD = process.env === 'production';
+let vite;
+
 async function createServer() {
   const app = express();
 
   // Create Vite server in middleware mode and configure the app type as
   // 'custom', disabling Vite's own HTML serving logic so parent server
   // can take control
-  console.log('IMPORT', import.meta);
-  const vite = await createViteServer({
-    mode: 'production',
-    server: { middlewareMode: true },
-    appType: 'custom',
-  });
+  console.log('PROCESS ENV', process.env.NODE_ENV);
+  if (!IS_PROD) {
+    vite = await createViteServer({
+      mode: 'production',
+      server: { middlewareMode: true },
+      appType: 'custom',
+    });
 
-  // Use vite's connect instance as middleware. If you use your own
-  // express router (express.Router()), you should use router.use
-  // When the server restarts (for example after the user modifies
-  // vite.config.js), `vite.middlewares` is still going to be the same
-  // reference (with a new internal stack of Vite and plugin-injected
-  // middlewares). The following is valid even after restarts.
-  app.use(vite.middlewares);
-
+    // Use vite's connect instance as middleware. If you use your own
+    // express router (express.Router()), you should use router.use
+    // When the server restarts (for example after the user modifies
+    // vite.config.js), `vite.middlewares` is still going to be the same
+    // reference (with a new internal stack of Vite and plugin-injected
+    // middlewares). The following is valid even after restarts.
+    app.use(vite.middlewares);
+  }
   app.use('*', async (req, res, next) => {
     const url = req.originalUrl;
 
@@ -49,7 +53,7 @@ async function createServer() {
         process.env.NODE_ENV === 'production'
           ? await import('../dist/server/entry-server.js')
           : await vite.ssrLoadModule('/src/entry-server.tsx');
-      console.log('SSR', process.env.NODE_ENV, imp);
+
       // 4. render the app HTML. This assumes entry-server.js's exported
       //     `render` function calls appropriate framework SSR APIs,
       //    e.g. ReactDOMServer.renderToString()
@@ -67,7 +71,9 @@ async function createServer() {
       // to your actual source code.
       console.log('Error ', e);
 
-      vite.ssrFixStacktrace(e);
+      if (!IS_PROD) {
+        vite.ssrFixStacktrace(e);
+      }
       next(e);
     }
   });
