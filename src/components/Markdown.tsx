2e8918afab9472eff1f0ee94fd35dd3cd99af53e
Moritz Roessler <morrossl@publicisgroupe.net>
2024-03-06 19:03:24 +0100

    fix: correct rsh for production

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -10,7 +10,7 @@ import Box from '@mui/material/Box';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { useContext, useEffect, useMemo, createElement } from 'react';
 import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { IconButton, List, ListItem, ListItemText } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import copy from 'copy-to-clipboard';
@@ -23,9 +23,7 @@ import { v4 } from 'uuid';
 import { Actions, stateContext } from '../provider/StateProvider';
 import { wrapPromise } from '../lib/util/SSR';
 
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
-registerLanguage('tsx', tsx);
-
+// import 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
 const getChildText = (props) => {
   const texts =
     props.children?.map?.((c) => {
